f99bc39902a4059e567cef91698a1f368fb1a86a
amavisto <amavisto@fd604f08-893b-0410-9d9c-6b8f4874c954>
2007-10-04 12:13:45 +0000

    nekoliko entiteta uvuceno u glavni kod i obrisano.

--- trunk/ppa_sk.vhd
+++ trunk/ppa_sk.vhd
@@ -1,31 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    21:24:58 10/02/2007 
--- Design Name: 
--- Module Name:    ppa_sk - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
----- Uncomment the following library declaration if instantiating
----- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity ppa_sk is
 	generic (n: integer; m: integer);
