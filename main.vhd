8a6d27bf77b12c60d18e074cfe10a0fdb713dad4
amavisto <amavisto@fd604f08-893b-0410-9d9c-6b8f4874c954>
2007-10-06 09:42:56 +0000

    druga implementacija importovana

---
+++ main.vhd
@@ -0,0 +1,77 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity main is
+port(A,B : in Std_Logic_Vector(8-1 downto 0); 
+	CI : in Std_Logic; 
+	S : out Std_Logic_Vector(8-1 downto 0); 
+	COUT : out Std_Logic); 
+end main;
+
+architecture Behavioral of main is
+component mux2x1 is
+    Port ( A : in  STD_LOGIC_VECTOR ;--unconstrained
+           B : in  STD_LOGIC_VECTOR ;
+           CTRL : in  STD_LOGIC;
+           OUTPUT : out  STD_LOGIC_VECTOR );
+end component;
+component konkatenator is
+    Port ( A : in  STD_LOGIC_VECTOR; --unconstrained
+           B : in  STD_LOGIC_VECTOR;
+           c : inout  STD_LOGIC_VECTOR);
+end component;
+
+signal C0,C1, BAFER1, BAFER2, BAFER3,BAFER4 : Std_Logic_Vector(8-1 downto 0); 
+TYPE matrica IS ARRAY (0 TO 3) OF STD_LOGIC_VECTOR(8-1 DOWNTO 0);
+SIGNAL  S0, S1: matrica;
+
+begin
+
+S0(0) <= A xnor B; 
+S1(0) <= not (A xnor B); 
+C0 <= A AND B;
+C1 <= A OR B;
+
+obrada: for k in 0 to 3-1 generate
+	
+	po_vrsti: for j in 1 to 2**(3-1-k) generate
+	
+	prvi_red: konkatenator 
+		port map(S0(k)(2*j-1 downto 2*j-1-((2**(k+1)-1)/2)),
+					S0(k)(2*j-1-((2**(k+1)-1)/2) downto 2*j-1-(2**(k+1)-1)),
+					BAFER1(2**(k+1)-1 downto 0));
+--	drugi_red: konkatenator 
+--		port map(S1(k)(2*j-1 downto 2*j-1-((2**(k+1)-1)/2)),
+--					S0(k)(2*j-1-((2**(k+1)-1)/2) downto 2*j-1-(2**(k+1)-1)),
+--					BAFER2(2**(k+1)-1 downto 0));
+--	desni_mux: mux2x1  port map(
+--					BAFER1(2**(k+1)-1 downto 0),
+--					BAFER2(2**(k+1)-1 downto 0),
+--					C0(2*j-1-1), 
+--					S0(k+1)(2*j-1 downto 2*j-1-(2**(k+1)-1)));
+					
+--treci_red: konkatenator 
+--		port map(S0(k)(2*j-1 downto 2*j-1-((2**(k+1)-1)/2)),
+--					S1(k)(2*j-1-((2**(k+1)-1)/2) downto 2*j-1-(2**(k+1)-1)),
+--					BAFER3(2**(k+1)-1 downto 0));
+--	cetvrti_red: konkatenator 
+--		port map(S1(k)(2*j-1 downto 2*j-1-((2**(k+1)-1)/2)),
+--					S1(k)(2*j-1-((2**(k+1)-1)/2) downto 2*j-1-(2**(k+1)-1)),
+--					BAFER4(2**(k+1)-1 downto 0));
+--	levi_mux: mux2x1  port map(
+--					BAFER3(2**(k+1)-1 downto 0),
+--					BAFER4(2**(k+1)-1 downto 0),
+--					C1(2*j-1-1), 
+--					S1(k+1)(2*j-1 downto 2*j-1-(2**(k+1)-1)));					
+	end generate po_vrsti;
+
+end generate obrada;
+
+--suma: mux2x1 port map(S0(3-1), S1(3-1), CI, S);
+--cout<= '1';
+
+end Behavioral;
+
--- main_test.vhd
+++ main_test.vhd
@@ -3,15 +3,15 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   22:03:09 10/02/2007
--- Design Name:   conditional_sum_adder
--- Module Name:   c:/apps/Xilinx/int_sab_cond/conditional_sum_test.vhd
--- Project Name:  int_sab_cond
+-- Create Date:   23:37:46 10/04/2007
+-- Design Name:   main
+-- Module Name:   D:/fax/03_algarh/04_projekat-conditional-sum_adder/impl_v2/main_test.vhd
+-- Project Name:  cond_sum_adder_v2
 -- Target Device:  
 -- Tool versions:  
 -- Description:   
 -- 
--- VHDL Test Bench Created by ISE for module: conditional_sum_adder
+-- VHDL Test Bench Created by ISE for module: main
 --
 -- Dependencies:
 -- 
@@ -31,58 +31,50 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.ALL;
 
-ENTITY conditional_sum_test_vhd IS
-END conditional_sum_test_vhd;
+ENTITY main_test_vhd IS
+END main_test_vhd;
 
-ARCHITECTURE behavior OF conditional_sum_test_vhd IS 
+ARCHITECTURE behavior OF main_test_vhd IS 
 
 	-- Component Declaration for the Unit Under Test (UUT)
-	COMPONENT conditional_sum_adder
+	COMPONENT main
 	port(A,B : in Std_Logic_Vector(7 downto 0); 
-	CI : in Std_Logic; 
-	SUB : in Std_Logic; 
-	S : out Std_Logic_Vector(7 downto 0); 
-	CO : out Std_Logic);
+		CI : in Std_Logic; 
+		S : out Std_Logic_Vector(7 downto 0); 
+		COUT : out Std_Logic); 
 	END COMPONENT;
 
 	--Inputs
 	SIGNAL CI :  std_logic := '0';
-	SIGNAL SUB :  std_logic := '0';
 	SIGNAL A :  std_logic_vector(7 downto 0) := (others=>'0');
 	SIGNAL B :  std_logic_vector(7 downto 0) := (others=>'0');
 
 	--Outputs
 	SIGNAL S :  std_logic_vector(7 downto 0);
-	SIGNAL CO :  std_logic;
+	SIGNAL COUT :  std_logic;
 
 BEGIN
 
 	-- Instantiate the Unit Under Test (UUT)
-	uut: conditional_sum_adder PORT MAP(
+	uut: main PORT MAP(
 		A => A,
 		B => B,
 		CI => CI,
-		SUB => SUB,
 		S => S,
-		CO => CO
+		COUT => COUT
 	);
 
 	tb : PROCESS
 	BEGIN
 
 		-- Wait 100 ns for global reset to finish
-		--wait for 100 ns;
+		wait for 100 ns;
+
 		-- Place stimulus here
-			A<= "00000000" ;
-			B<= "00000000" ;
-			A<= "00000001" after 10ns;
-			A<= "00000011" after 20ns;
-			B<= "00000001" after 30ns;
-			B<= "00000010" after 40ns;
-			B<= "00000001" after 50ns;
-			B<= "00001011" after 60ns;
-			--A<= "00000000" after 90ns;
-			--B<= "00000000" after 90ns;
+		A<="00000001" after 10ns;
+	   A<="00000011" after 20ns;
+		B<="00000010" after 30ns;
+		B<="00000101" after 40ns;
 		wait; -- will wait forever
 	END PROCESS;
 
