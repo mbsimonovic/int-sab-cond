b0b04b96a723caf9a8384fae10b4edd78d4c22d5
amavisto <amavisto@fd604f08-893b-0410-9d9c-6b8f4874c954>
2007-10-09 19:19:16 +0000

    v1. uspesno zavrsena. sabirac ima i overflow flag a moze da radi i kao oduzimac. kod razbijen na komponente.

--- mux2x1.vhd
+++ mux2x1.vhd
@@ -1,24 +1,27 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+-- Multiplekser tipa 2 u 1 sa proizvoljnom duzinom ulaznih signala. 
+-- Za CTRL='1' na izlazu se pojavljuje signal A, za CTRL='0' signal B.
 entity mux2x1 is
-
-    Port ( A : in  STD_LOGIC_VECTOR ; -- Unconstrained array
-           B : in  STD_LOGIC_VECTOR ;
-           CTRL : in  STD_LOGIC;
-           OUTPUT : out  STD_LOGIC_VECTOR );
+    Port ( A : in  STD_LOGIC_VECTOR ; 			-- prvi ulazni signal
+           B : in  STD_LOGIC_VECTOR ;			-- drugi ulazni signal
+           CTRL : in  STD_LOGIC;					-- kontrolni signal
+           OUTPUT : out  STD_LOGIC_VECTOR ); -- izlazni signal
 end mux2x1;
 
 architecture Behavioral of mux2x1 is
 
 begin
-process (a,b,ctrl)
-begin
-	if (CTRL = '1') then
-		OUTPUT <= A;
-	else 
-		OUTPUT <= B;
-	end if;
-end process;
+	OUTPUT <= A WHEN CTRL ='1' else B;
+--alternativno:
+--	process (a,b,ctrl)
+--	begin
+--		if (CTRL = '1') then
+--			OUTPUT <= A;
+--		else 
+--			OUTPUT <= B;
+--		end if;
+--	end process;
 end Behavioral;
 
---
+++ preprocessor.vhd
@@ -0,0 +1,40 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    20:14:00 10/09/2007 
+-- Design Name: 
+-- Module Name:    preprocessor - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.components.all;
+
+-- komponenta koja racuna sume i prenose za svaku bitsku poziciju
+entity preprocessor is
+	port(A,B : in Std_Logic_Vector; 
+	ZeroSum, OneSum, ZeroCarry, OneCarry: out Std_Logic_Vector);
+end preprocessor;
+
+architecture Behavioral of preprocessor is
+
+begin
+
+	ZeroSum <= A xor B; 
+	OneSum<= not (A xor B);
+	ZeroCarry <= A AND B;
+	OneCarry <= A OR B;
+
+end Behavioral;
+
